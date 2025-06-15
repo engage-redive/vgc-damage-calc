a9eadea5345fff7e96294a9b14b03dd025e68147
Your Name <you@example.com>
2025-06-15 20:46:34 +0900

    Move image assets to public folder and update paths

--- src/data/typeEffectiveness.ts
+++ src/data/typeEffectiveness.ts
@@ -14,7 +14,7 @@ export const typeEffectiveness: Record<PokemonType, Record<PokemonType, number>>
     psychic: { fighting: 2, poison: 2, steel: 0.5, psychic: 0.5, dark: 0, normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, ground: 1, flying: 1, bug: 1, rock: 1, ghost: 1, dragon: 1, fairy: 1 },
     bug: { grass: 2, psychic: 2, dark: 2, fire: 0.5, fighting: 0.5, poison: 0.5, flying: 0.5, ghost: 0.5, steel: 0.5, fairy: 0.5, normal: 1, water: 1, electric: 1, ice: 1, ground: 1, bug: 1, rock: 1, dragon: 1 },
     rock: { fire: 2, ice: 2, flying: 2, bug: 2, fighting: 0.5, ground: 0.5, steel: 0.5, normal: 1, water: 1, electric: 1, grass: 1, poison: 1, psychic: 1, rock: 1, ghost: 1, dragon: 1, dark: 1, fairy: 1 },
-    ghost: { psychic: 2, ghost: 2, dark: 0.5, normal: 0, fighting: 0, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, poison: 1, ground: 1, flying: 1, bug: 1, rock: 1, steel: 1, dragon: 1, fairy: 1 },
+    ghost: { psychic: 2, ghost: 2, dark: 0.5, normal: 0, fighting: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, poison: 1, ground: 1, flying: 1, bug: 1, rock: 1, steel: 1, dragon: 1, fairy: 1 },
     dragon: { dragon: 2, steel: 0.5, fairy: 0, normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 1, dark: 1 },
     dark: { psychic: 2, ghost: 2, fighting: 0.5, dark: 0.5, fairy: 0.5, normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, poison: 1, ground: 1, flying: 1, bug: 1, rock: 1, steel: 1, dragon: 1 },
     steel: { ice: 2, rock: 2, fairy: 2, steel: 0.5, fire: 0.5, water: 0.5, electric: 0.5, normal: 1, grass: 1, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, ghost: 1, dragon: 1, dark: 1 },
